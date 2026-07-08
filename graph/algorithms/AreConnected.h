d43b9dc8f17295e7fb14fd7875b7e8649cd4220b
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 00:12:57 +0330

    develope AreConnected.h

--- graph/algorithms/AreConnected.h
+++ graph/algorithms/AreConnected.h
@@ -25,3 +25,22 @@ namespace nexora::graph::algorithms {
         using VisitedMap = std::unordered_map<DenseId, int>;
         using BfsQueue   = std::deque<DenseId>;
 
+        static bool expandLevel(const LiveGraph& graph,
+                                BfsQueue&        queue_active,
+                                VisitedMap&      visited_active,
+                                const VisitedMap& visited_other,
+                                size_t           level_size);
+
+
+        static std::string buildJson(bool               connected,
+                                     int                hops,
+                                     const std::string& u1,
+                                     const std::string& u2);
+
+
+        static double elapsedMs(
+                const std::chrono::steady_clock::time_point& t0);
+    };
+
+}
+
