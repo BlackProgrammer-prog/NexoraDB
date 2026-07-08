72513f496e008b807c39d9d20b7eb6ac766514da
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-08 23:24:04 +0330

    develope AreConnected.h

--- graph/algorithms/AreConnected.h
+++ graph/algorithms/AreConnected.h
@@ -2,10 +2,6 @@
 // Created by Acer on 7/8/2026.
 //
 
-#ifndef NEXORADB_ARECONNECTED_H
-#define NEXORADB_ARECONNECTED_H
-
-#endif //NEXORADB_ARECONNECTED_H
 #pragma once
 #include "AlgorithmBase.h"
 #include <chrono>
