5743f2254e8046304b1c6d7433af16069ebf15bd
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 01:09:37 +0330

    develope InfluenceMaximization.h

--- graph/algorithms/InfluenceMaximization.h
+++ graph/algorithms/InfluenceMaximization.h
@@ -9,3 +9,11 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+namespace nexora::graph::algorithms {
+
+    class InfluenceMaximization final : public JobAlgorithm {
+    public:
+        std::string name() const override;
+
+        AlgoResult run(const StaticGraph&         snapshot,
+                       const std::vector<ExtId>& params) override;
