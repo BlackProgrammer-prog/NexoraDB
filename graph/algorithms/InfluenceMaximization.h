886be34f89c610dedc083ddc6a183d2602bc93d1
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 01:09:12 +0330

    develope InfluenceMaximization.h

--- graph/algorithms/InfluenceMaximization.h
+++ graph/algorithms/InfluenceMaximization.h
@@ -6,3 +6,11 @@
 #define NEXORADB_INFLUENCEMAXIMIZATION_H
 
 #endif //NEXORADB_INFLUENCEMAXIMIZATION_H
+#pragma once
+#include "AlgorithmBase.h"
+#include <deque>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
