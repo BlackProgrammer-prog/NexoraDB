9a9c5a17c18a7cf49bd94b29b7e6d2d54145c9f3
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-08 23:11:49 +0330

    develope GetFriends.h

--- graph/algorithms/GetFriends.h
+++ graph/algorithms/GetFriends.h
@@ -17,3 +17,23 @@ namespace nexora::graph::algorithms {
 
         AlgoResult run(const LiveGraph&            graph,
                        const std::vector<ExtId>&  params) override;
+
+    private:
+
+        static constexpr size_t kMaxLimit = 10'000;
+
+
+        static void collectNeighbors(const LiveGraph&      graph,
+                                     DenseId               uid,
+                                     TypeId                edge_type,
+                                     size_t                limit,
+                                     std::vector<DenseId>& out_vec);
+
+
+        static std::string buildJson(const LiveGraph&            graph,
+                                     const ExtId&                user_id,
+                                     const std::vector<DenseId>& friends,
+                                     bool                        limit_applied);
+    };
+
+}
