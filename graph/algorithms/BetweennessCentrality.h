58cf0658c1a60c004ea0db0373c6384d2f9ecca6
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 00:51:44 +0330

    develope BetweennessCentrality.h

--- graph/algorithms/BetweennessCentrality.h
+++ graph/algorithms/BetweennessCentrality.h
@@ -1,11 +1,6 @@
 //
 // Created by Acer on 7/9/2026.
 //
-
-#ifndef NEXORADB_BETWEENNESSCENTRALITY_H
-#define NEXORADB_BETWEENNESSCENTRALITY_H
-
-#endif //NEXORADB_BETWEENNESSCENTRALITY_H
 #pragma once
 #include "AlgorithmBase.h"
 #include <deque>
