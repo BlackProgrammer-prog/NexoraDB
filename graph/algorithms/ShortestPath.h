fa7465f624248fcaf41ffdd04698d8347ae0327d
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 00:23:06 +0330

    develope ShortestPath.h

--- graph/algorithms/ShortestPath.h
+++ graph/algorithms/ShortestPath.h
@@ -17,3 +17,16 @@ namespace nexora::graph::algorithms {
 
         AlgoResult run(const LiveGraph&           graph,
                        const std::vector<ExtId>& params) override;
+
+    private:
+        static constexpr int kMaxDepth = 12;
+        using ParentMap = std::unordered_map<DenseId, DenseId>;
+        using BfsQueue  = std::deque<DenseId>;
+
+        static bool expandLevel(const LiveGraph& graph,
+                                BfsQueue&        q_active,
+                                ParentMap&       p_active,
+                                const ParentMap& p_other,
+                                size_t           level_size,
+                                DenseId&         meet_out);
+
