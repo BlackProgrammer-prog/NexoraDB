b054cf34eb45826818f9a6485d205ea97998521c
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 00:43:30 +0330

    develope FriendSuggestion.h

--- graph/algorithms/FriendSuggestion.h
+++ graph/algorithms/FriendSuggestion.h
@@ -20,7 +20,17 @@ namespace nexora::graph::algorithms {
     private:
         static constexpr size_t kDefaultLimit = 10;
         static constexpr size_t kMaxLimit     = 1'000;
-        
+
         using FilterSet     = std::unordered_set<DenseId>;
         using ScoreMap      = std::unordered_map<DenseId, int>;
         using RankedVec     = std::vector<std::pair<int, DenseId>>;
+
+        static void gatherNeighbors(const LiveGraph& graph,
+                                    DenseId          uid,
+                                    FilterSet&       filter_set);
+
+        static ScoreMap countMutual(const LiveGraph& graph,
+                                    const FilterSet& direct_friends,
+                                    DenseId          uid);
+
+        static RankedVec rankAndTrim(const ScoreMap& scores, size_t limit);
