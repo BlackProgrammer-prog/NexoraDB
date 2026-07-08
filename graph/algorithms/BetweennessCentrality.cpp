7805c8329b1d07ddb25023b0d8f482406932c54c
meysam-nikoogoftar <meysammohseny0011@gmail.com>
2026-07-09 00:57:26 +0330

    develope BetweennessCentrality.cpp

--- graph/algorithms/BetweennessCentrality.cpp
+++ graph/algorithms/BetweennessCentrality.cpp
@@ -7,3 +7,13 @@
 #include <iomanip>
 #include <numeric>
 #include <sstream>
+namespace nexora::graph::algorithms {
+
+
+    BetweennessCentrality::BfsState::BfsState(size_t N, size_t s_idx)
+    : pred(N), sigma(N, 0.0), dist(N, -1)
+    {
+        sigma[s_idx] = 1.0;
+        dist[s_idx]  = 0;
+        queue.push_back(s_idx);
+    }
